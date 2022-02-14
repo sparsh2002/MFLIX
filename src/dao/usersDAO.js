55bc086d0800aa828b4ee2a5b8aa5aa7baeae242
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-02-14 14:31:41 +0530

    user management done

--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -39,7 +39,7 @@ export default class UsersDAO {
   static async getUser(email) {
     // TODO Ticket: User Management
     // Retrieve the user document corresponding with the user's email.
-    return await users.findOne({ someField: "someValue" })
+    return await users.findOne({ email })
   }
 
   /**
@@ -60,8 +60,10 @@ export default class UsersDAO {
       // Insert a user with the "name", "email", and "password" fields.
       // TODO Ticket: Durable Writes
       // Use a more durable Write Concern for this operation.
-      await users.insertOne({ someField: "someValue" })
+
+      await users.insertOne({ ...userInfo })
       return { success: true }
+
     } catch (e) {
       if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
         return { error: "A user with the given email already exists." }
@@ -82,11 +84,14 @@ export default class UsersDAO {
       // TODO Ticket: User Management
       // Use an UPSERT statement to update the "jwt" field in the document,
       // matching the "user_id" field with the email passed to this function.
+
       await sessions.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        { user_id: email },
+        { $set: { jwt } },
+        { upsert: true }
       )
       return { success: true }
+
     } catch (e) {
       console.error(`Error occurred while logging in user, ${e}`)
       return { error: e }
@@ -102,8 +107,10 @@ export default class UsersDAO {
     try {
       // TODO Ticket: User Management
       // Delete the document in the `sessions` collection matching the email.
-      await sessions.deleteOne({ someField: "someValue" })
+
+      await sessions.deleteOne({ user_id: email })
       return { success: true }
+      
     } catch (e) {
       console.error(`Error occurred while logging out user, ${e}`)
       return { error: e }
@@ -120,7 +127,9 @@ export default class UsersDAO {
     try {
       // TODO Ticket: User Management
       // Retrieve the session document corresponding with the user's email.
-      return sessions.findOne({ someField: "someValue" })
+
+      return sessions.findOne({ user_id: email })
+
     } catch (e) {
       console.error(`Error occurred while retrieving user session, ${e}`)
       return null
@@ -168,9 +177,11 @@ export default class UsersDAO {
 
       // TODO Ticket: User Preferences
       // Use the data in "preferences" to update the user's preferences.
+
+      // here's how the update statement is implemented
       const updateResponse = await users.updateOne(
-        { someField: someValue },
-        { $set: { someOtherField: someOtherValue } },
+        { email },
+        { $set: { preferences } },
       )
 
       if (updateResponse.matchedCount === 0) {
