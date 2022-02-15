916d937b217e4e03290b22761b7f84d852901f7a
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-02-15 10:32:05 +0530

    sorted user on the basis of number of comments

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -126,13 +126,16 @@ export default class CommentsDAO {
     email in the `comments` collection.
     */
     try {
+      const groupStage = { $group: { _id: "$email", count: { $sum: 1 } } }
+      const sortStage = { $sort: { count: -1 } }
+      const limitStage = { $limit: 20 }
+      const pipeline = [groupStage, sortStage, limitStage]
       // TODO Ticket: User Report
       // Return the 20 users who have commented the most on MFlix.
-      const pipeline = []
 
       // TODO Ticket: User Report
       // Use a more durable Read Concern here to make sure this data is not stale.
-      const readConcern = comments.readConcern
+      const readConcern = { level: "majority" }
 
       const aggregateResult = await comments.aggregate(pipeline, {
         readConcern,
